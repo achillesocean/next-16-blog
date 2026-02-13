451c01c70f7c2d1d42c296282251947f6454432d
Felmeta Dereje <0202sent@gmail.com>
2026-02-13 14:40:47 +0300

    init with navbar

---
+++ components/web/navbar.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+
+type Props = {};
+
+export function Navbar({}: Props) {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Blog</span>
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-2">
+          <Link
+            href="/"
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            Home
+          </Link>
+          <Link
+            href="/blog"
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            Blog
+          </Link>
+          <Link
+            href="/create"
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            Create
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link href="/auth/signup" className={buttonVariants()}>
+          Sign Up
+        </Link>
+        <Link
+          href="/auth/login"
+          className={buttonVariants({
+            variant: "secondary",
+          })}
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
