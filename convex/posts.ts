0d0d592b7e2afe12e0a0e1b2d7fbfe7ed8db3654
Felmeta Dereje <0202sent@gmail.com>
2026-02-23 01:54:20 +0300

    starting point for blog page route

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,6 +1,7 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { authComponent } from "./auth";
+import { createVerify } from "crypto";
 
 export const createPost = mutation({
   args: {
@@ -48,5 +49,27 @@ export const generateImageUploadUrl = mutation({
     }
 
     return await ctx.storage.generateUploadUrl();
+  },
+});
+
+export const getPostById = query({
+  args: {
+    postId: v.id("posts"),
+  },
+  handler: async (ctx, args) => {
+    const post = await ctx.db.get(args.postId);
+
+    if (!post) {
+      return null;
+    }
+    const resolvedImageUrl =
+      post?.imageStorageId !== undefined
+        ? await ctx.storage.getUrl(post.imageStorageId)
+        : null;
+
+    return {
+      ...post,
+      imageUrl: resolvedImageUrl,
+    };
   },
 });
